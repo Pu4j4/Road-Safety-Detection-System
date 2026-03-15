5accbe5fa3a5abb276b015b2a2c6a4ef37a04a47
pu4j4 <bhanubethala2@gmail.com>
2026-03-15 19:46:36 +0530

    remove: cleared all files for fresh selective push

--- road_safety_detection_system/wsgi.py
+++
@@ -1,5 +0,0 @@
-import os
-from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'road_safety_detection_system.settings')
-application = get_wsgi_application()
