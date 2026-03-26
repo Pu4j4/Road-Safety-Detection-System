09cb5c7bcbfce8e8c6e30aeb9dad6b9840c64b5e
pu4j4 <bhanubethala2@gmail.com>
2026-03-26 13:55:58 +0530

    updated download_models.py

--- download_models.py
+++ download_models.py
@@ -2,29 +2,21 @@ import os
 import gdown
 
 LANE_MODEL_ID    = '1nsoLxwYR0kr41SfO-xcjKLaIJsJ6R2x1'  # lane_model.onnx
-POTHOLE_MODEL_ID = '1EjE3HGr9AttSLCXnKnZnR74y35FmGlgg'  # best.pt
+POTHOLE_MODEL_ID = '17BtCQOlgec16jVqsD5lTfCCtvan9idiE'  # best.onnx
 
 def download_models():
     os.makedirs('model', exist_ok=True)
 
     if not os.path.exists('model/lane_model.onnx'):
-        print('⬇ Downloading lane model (ONNX) from Google Drive...')
-        gdown.download(
-            id=LANE_MODEL_ID,
-            output='model/lane_model.onnx',
-            quiet=False
-        )
+        print('⬇ Downloading lane model...')
+        gdown.download(id=LANE_MODEL_ID, output='model/lane_model.onnx', quiet=False)
         print('✅ Lane model downloaded.')
     else:
         print('✅ Lane model already exists.')
 
-    if not os.path.exists('model/best.pt'):
-        print('⬇ Downloading pothole model from Google Drive...')
-        gdown.download(
-            id=POTHOLE_MODEL_ID,
-            output='model/best.pt',
-            quiet=False
-        )
+    if not os.path.exists('model/best.onnx'):
+        print('⬇ Downloading pothole model...')
+        gdown.download(id=POTHOLE_MODEL_ID, output='model/best.onnx', quiet=False)
         print('✅ Pothole model downloaded.')
     else:
         print('✅ Pothole model already exists.')
